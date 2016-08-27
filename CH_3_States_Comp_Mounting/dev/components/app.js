65275a97dd25cbb725fdc6992faa95df592ae2a7
Sunil <skumar225@sapient.com>
2016-08-27 17:31:03 +0100

    Improved chapter 3

---
+++ CH_3_States_Comp_Mounting/dev/components/app.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+
+    constructor(props) {
+        super();
+
+        this.state = {
+         name: "Sunil Syal",
+         passion: props.passion,
+         age: 38
+        };
+    }
+
+    componentWillUpdate(newProps, newState) {
+      console.log("===== componentWillUpdate");
+      console.log(newProps, newState);
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+      console.log("===== componentDidUpdate");
+      console.log(prevProps, prevState);
+    }
+
+    incAge () {
+      //this.state.age++;
+      var age = this.state.age + 1;
+      this.setState({age});
+    }
+
+    componentWillMount () {
+      console.log("Will Mount");
+    }
+
+    componentDidMount () {
+      console.log("Mounted");
+    }
+
+    render() {
+        return (
+          <div>
+
+            { /* Read state */ }
+
+            <p> Name: { this.state.name }</p>
+            <p> Passion: { this.state.passion }</p>
+            <p> Age: { this.state.age }</p>
+
+            <button onClick={this.incAge.bind(this)}>Age ++ </button>
+
+          </div >
+        );
+    }
+}
